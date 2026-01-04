fb0d95c6cd44cdc9d727af9d034c0f559e4e4a1b
Sahil Yatam <sahilyatam9@gmail.com>
2026-01-04 16:19:09 +0530

    chore(shared): add shared type definitions

---
+++ backend/src/shared/types/express.d.ts
@@ -0,0 +1,9 @@
+import { UserDocument } from "../../modules/user/user.model.ts";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: UserDocument;
+        }
+    }
+}
