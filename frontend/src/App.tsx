e317d040ec3b725ecd9d3f3260a3a85694336770
Sahil Yatam <sahilyatam9@gmail.com>
2026-02-07 23:12:14 +0530

    feat: set up React app entry and global styles

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./pages/Home"
+
+
+function App() {
+  
+
+  return (
+    <>
+        <div className='p-4 h-screen flex items-center justify-center'>
+            <Home/>
+        </div>
+    </>
+  )
+}
+
+export default App
