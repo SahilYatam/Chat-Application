24a2f94bd35e0eaf4d7f2fa4e31cb6d2cc01fca7
Sahil Yatam <sahilyatam9@gmail.com>
2026-02-17 18:37:02 +0530

    refactor(frontend): update app entry point and global styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,56 @@
-import Home from "./pages/Home"
+import Home from "./pages/Home";
+import { connectSocket, disconnectSocket } from "./socket/socket";
+import { useEffect } from "react";
+import { registerChatSocketEvents } from "./features/chat/chatSocket";
+import { useAppSelector } from "./store/hooks";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LoginPage } from "./pages/Login";
+import { SignupPage } from "./pages/Signup";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home/>,
+    },
+
+    {
+        path: "/login",
+        element: <LoginPage/>,
+    },
+
+    {
+        path: "/signup",
+        element: <SignupPage/>,
+    },
+
+
+])
 
 
 function App() {
-  
+    const { accessToken, user } = useAppSelector((state) => state.auth);
 
-  return (
-    <>
-        <div className='p-4 h-screen flex items-center justify-center'>
-            <Home/>
-        </div>
-    </>
-  )
+    useEffect(() => {
+        if (!accessToken || !user) return;
+
+        const socket = connectSocket(accessToken, user._id);
+        console.log("🟢 SOCKET CONNECTED", socket.id);
+        registerChatSocketEvents(socket);
+
+        return () => {
+            disconnectSocket();
+        };
+    }, [accessToken, user]);
+
+    
+
+    return (
+        <>
+            <div className="p-4 h-screen flex items-center justify-center">
+                <RouterProvider router={router}/>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
